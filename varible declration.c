c7e2c8beb2a91bfc0abb8e5642aed1a986de9ede
Ram1319 <86654826+Ram1319@users.noreply.github.com>
2021-10-30 10:30:06 +0530

    Add files via upload

---
+++ varible declration.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ void main()
+ {
+ 	//varible declaration
+ 	int maths,english,chemistry,physics,sum;
+ 	//instalization
+ 	maths=50;
+ 	english=75;
+ 	chemistry=55;
+ 	physics=67;
+ 	//calculization
+ 	sum=maths+english+chemistry+physics;
+ 	printf("maths%d\nenglish%d\nchemistry%d\nphysics%d\ntotal%d\n",maths,english,chemistry,physics,sum);
+ }
