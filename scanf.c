ba4b4abb7bafc8cea57974ddb7a260dcaa8fb259
Ram1319 <86654826+Ram1319@users.noreply.github.com>
2021-11-11 10:09:13 +0530

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,9 @@
+//program to print product of numbers
+#include<stdio.h>
+void main()
+{
+	int a, b, c, d;
+	printf("Enter Four numbers:");
+	scanf("%d%d%d%d", &a, &b, &c, &d);
+	printf("%d", a*b*c*d);
+}
