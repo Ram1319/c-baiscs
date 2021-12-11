5857c6b168cd32077d59ebfd32659d675f970081
Ram1319 <86654826+Ram1319@users.noreply.github.com>
2021-12-11 10:38:31 +0530

    Add files via upload

---
+++ conditional statements2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("Enter the number:");
+	scanf("%d", &number);
+	if(number%2==0)
+	{
+		printf("The given number is even");
+	}
+	else
+	{
+		printf("The given number is odd");
+	}
+}
